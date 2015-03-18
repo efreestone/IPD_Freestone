c844eb40e29f8358f1a93a3595f982b827f06621
efreestone <efreestone@fullsail.edu>
2015-03-18 15:27:59 -0600

    Link issue was not fixed and was related to my having spaces in my original project name. Not sure what I was thinking having spaces to begin with but rebuilt the project to fix instead of dealing with cleaning up after a project rename. Just easier since I'm not far yet.

---
+++ MyBrewLogV1/MyBrewLogV1/FirstViewController.m
@@ -0,0 +1,36 @@
+// Elijah Freestone
+// IPD1 1503
+// Week 3
+// March 18th, 2015
+
+//
+//  FirstViewController.m
+//  MyBrewLogV1
+//
+//  Created by Elijah Freestone on 3/18/15.
+//  Copyright (c) 2015 Elijah Freestone. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    //Test parse
+    PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
+    testObject[@"foo"] = @"bar";
+    [testObject saveInBackground];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
