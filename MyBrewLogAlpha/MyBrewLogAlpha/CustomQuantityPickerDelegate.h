a73d8b055be1a43ff11263d98de115ca41b6f56d
efreestone <efreestone@fullsail.edu>
2015-04-02 20:20:18 -0600

    Built custome quantity delegate based on timer del and the other cusome delegate I've been working on. Found some odd behaviour with returning a value with the default delegate methods but came up with a good work-around.

---
+++ MyBrewLogAlpha/MyBrewLogAlpha/CustomQuantityPickerDelegate.h
@@ -0,0 +1,13 @@
+//
+//  CustomQuantityPickerDelegate.h
+//  MyBrewLogAlpha
+//
+//  Created by Elijah Freestone on 4/2/15.
+//  Copyright (c) 2015 Elijah Freestone. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CustomQuantityPickerDelegate : NSObject
+
+@end
---
+++ MyBrewLogAlpha/MyBrewLogAlpha/CustomQuantityPickerDelegate.m
@@ -0,0 +1,13 @@
+//
+//  CustomQuantityPickerDelegate.m
+//  MyBrewLogAlpha
+//
+//  Created by Elijah Freestone on 4/2/15.
+//  Copyright (c) 2015 Elijah Freestone. All rights reserved.
+//
+
+#import "CustomQuantityPickerDelegate.h"
+
+@implementation CustomQuantityPickerDelegate
+
+@end
